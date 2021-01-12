90dd370acf4834fdf93079c4e215deb0fa5617fe
Brian Michael <bm250290@ncr.com>
2021-01-12 10:58:07 -0500

    Sprint initializr

---
+++ src/main/java/org/eaa690/aerie/AerieApplication.java
@@ -0,0 +1,13 @@
+package org.eaa690.aerie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AerieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AerieApplication.class, args);
+	}
+
+}
